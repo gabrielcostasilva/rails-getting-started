a036c6822290b893cdc0448dff428e9f7e44f884
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2024-10-15 10:23:33 -0300

    Debugging controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,16 +3,18 @@ class CommentsController < ApplicationController
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
-
+        
         redirect_to article_path(@article)
     end
-
+    
     def destroy
         @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
-
+        
         @comment.destroy
 
+        logger.tagged("COMMENT") { logger.info("Comment has been destroyed!") }
+        
         redirect_to article_path(@article), status: :see_other
     end
 
