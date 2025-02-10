c6ec2b455da79bad809c007be891f8a77252477e
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2025-02-10 15:28:16 -0300

    Profiled operation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,24 @@
 class CommentsController < ApplicationController
-
     def create
-        @article = Article.find(params[:article_id])
+        Rack::MiniProfiler.step("Retrieving data") do
+            @article = Article.find(params[:article_id])
+        end
+
         @comment = @article.comments.create(comment_params)
 
         redirect_to article_path(@article)
     end
 
     def destroy
-        @article = Article.find(params[:article_id])
-        @comment = @article.comments.find(params[:id])
+            @article = Article.find(params[:article_id])
+            @comment = @article.comments.find(params[:id])
 
         @comment.destroy
 
         redirect_to article_path(@article), status: :see_other
     end
 
-    private 
+    private
         def comment_params
             params.require(:comment).permit(:commenter, :body, :status)
         end
